e504991e21125c368c179345306c707d04e4b548
hungtan1406 <hungtanth1406@gmail.com>
2026-01-09 18:01:22 +0700

    completed feature login/logout

--- src/app/admin/(dashboard)/layout.tsx
+++ src/app/admin/(dashboard)/layout.tsx
@@ -1,27 +1,63 @@
+'use client';
 import AdminSidebar from '@/components/admin/Sidebar';
 import AdminTopbar from '@/components/admin/AdminTopbar';
 import { Toaster } from 'sonner';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
 
 export default function AdminLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <div className="flex min-h-screen bg-gray-50/50">
-            {/* Sidebar */}
-            <AdminSidebar />
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
+  const router = useRouter();
 
-            <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
-                {/* Topbar */}
-                <AdminTopbar />
+  useEffect(() => {
+    const checkToken = () => {
+      const token = Cookies.get('accessToken');
+      if (!token) {
+        router.push('/admin/login');
+      } else {
+        try {
+          const decodedToken = JSON.parse(atob(token.split('.')[1]));
+          const expirationTime = decodedToken.exp * 1000;
 
-                {/* Content Area */}
-                <main className="flex-1 overflow-y-auto p-8 custom-scrollbar">
-                    {children}
-                </main>
-            </div>
-            <Toaster richColors position="top-right" />
-        </div>
-    );
+          if (expirationTime < Date.now()) {
+            Cookies.remove('accessToken');
+            router.push('/admin/login');
+          } else {
+            setIsAuthenticated(true);
+          }
+        } catch (error) {
+          Cookies.remove('accessToken');
+          router.push('/admin/login');
+        }
+      }
+    };
+
+    checkToken();
+  }, [router]);
+
+  if (isAuthenticated === null) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div className='flex min-h-screen bg-gray-50/50'>
+      {/* Sidebar */}
+      <AdminSidebar />
+
+      <div className='flex-1 flex flex-col min-w-0 overflow-hidden'>
+        {/* Topbar */}
+        <AdminTopbar />
+
+        {/* Content Area */}
+        <main className='flex-1 overflow-y-auto p-8 custom-scrollbar'>
+          {children}
+        </main>
+      </div>
+      <Toaster richColors position='top-right' />
+    </div>
+  );
 }
